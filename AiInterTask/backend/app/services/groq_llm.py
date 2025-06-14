634ab5d3532f060202e0aa0fca11cea0546724ae
Shivam Dangwal <shivamdangwal04@gmail.com>
2025-06-14 16:27:44 +0530

    Updating load_dotenv() in groq_llm.py

--- AiInterTask/backend/app/services/groq_llm.py
+++ AiInterTask/backend/app/services/groq_llm.py
@@ -1,6 +1,14 @@
 from openai import OpenAI
 import json
-from app.core.config import GROQ_API_KEY
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY not set")
 
 client = OpenAI(
     base_url="https://api.groq.com/openai/v1",
